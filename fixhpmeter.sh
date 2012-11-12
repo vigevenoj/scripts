7019be3eb1db9b105614b3d4b0b1603b63a3ae54
Jacob Vigeveno <vigevenoj@gmail.com>
2012-11-11 19:17:32 -0800

    Remove -r option from sed for osx compat. Clean up seds

--- fixhpmeter.sh
+++ fixhpmeter.sh
@@ -2,6 +2,7 @@
 # test for terminal from https://gist.github.com/1966557
 
 GREP_STRING="Rescan (parallel)\|^:" # Don't output these lines
+SED_NO_PRINTGCDATESTAMPS="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+0000: " # Strip -XX:+PrintGCDateStamps output from the log
 OUTPUT_FILENAME=fixed-gc.vgc
 RENAME_PATTERN="s/-gc\(.*\).log/\1.vgc/g"
 
@@ -13,9 +14,9 @@ if [[ -t 0 ]]; then
 	if [[ -n $2 ]]; then
 		OUTPUT_FILENAME=$2
 	fi
-	cat -vet $1 | grep -v "\^@" | grep -v "$GREP_STRING" | sed  -e 's/\$//g' -re 's/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+0000: //g' > $OUTPUT_FILENAME
+	cat -vet $1 | grep -v "\^@" | grep -v "$GREP_STRING" | sed  -e 's/\$//g' -e 's/'"$SED_NO_PRINTGCDATESTAMPS"'//g' > $OUTPUT_FILENAME
 	echo "Wrote output to $OUTPUT_FILENAME"
 else
 	# default behavior, mostly same as old script
-	cat -vet | grep -v "$GREP_STRING" | sed 's/\$//g' -re 's/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+0000: //g'  > $OUTPUT_FILENAME
+	cat -vet | grep -v "$GREP_STRING" | sed 's/\$//g' -e 's/'"$SED_NO_PRINTGCDATESTAMPS"'//g'  > $OUTPUT_FILENAME
 fi
