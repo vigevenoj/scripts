d6c64377a1a10f3d661885a075ad9d9ef01b3830
Jacob Vigeveno <jacob.vigeveno@jivesoftware.com>
2012-12-03 20:17:06 -0800

    Pass the right flag for seds. Cleanup.

--- fixhpmeter.sh
+++ fixhpmeter.sh
@@ -6,20 +6,39 @@ GREP_STRING="Rescan (parallel)\|^:"
 # SED_NO_PRINTGCDATESTAMPS will strip -XX:+PrintGCDateStamps output from the log. Requires passing -r to GNU sed
 SED_NO_PRINTGCDATESTAMPS="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+0000: " 
 
+
+SEDFLAG=
+
+# GNU sed needs -r, BSD sed needs -E
+# Macports' gsed is GNU sed, OS X default sed is BSD, so this test works
+function sedflag () {
+	case `uname` in
+		Linux)
+			SEDFLAG='-r'
+			;;
+		Darwin)
+			SEDFLAG='-E'
+			;;
+	esac
+}
+
+function assembleOutputFilename() {
 OUTPUT_FILENAME=fixed-gc.vgc
 RENAME_PATTERN="s/-gc\(.*\).log/\1.vgc/g"
-
-if [[ -t 0 ]]; then
-	# Reading from a terminal
-	if [[ $1 =~ .*-gc.*.log ]]; then
+	if [[ $1 =~ .*-gc*.log ]]; then
 		OUTPUT_FILENAME=`echo $1 | sed "$RENAME_PATTERN"`
 	fi
 	if [[ -n $2 ]]; then
 		OUTPUT_FILENAME=$2
 	fi
-	cat -vet $1 | grep -v "\^@" | grep -v "$GREP_STRING" | sed  -e 's/\$//g' -re 's/'"$SED_NO_PRINTGCDATESTAMPS"'//g' > $OUTPUT_FILENAME
+}
+
+if [[ -t 0 ]]; then # Reading from a terminal
+	sedflag
+	assembleOutputFilename $1 $2
+	cat -vet $1 | grep -v "\^@" | grep -v "$GREP_STRING" | sed $SEDFLAG  -e 's/\$//g' -e 's/'"$SED_NO_PRINTGCDATESTAMPS"'//g' > $OUTPUT_FILENAME
 	echo "Wrote output to $OUTPUT_FILENAME"
 else
 	# default behavior, mostly same as old script
-	cat -vet | grep -v "$GREP_STRING" | sed 's/\$//g' -re 's/'"$SED_NO_PRINTGCDATESTAMPS"'//g'  > $OUTPUT_FILENAME
+	cat -vet | grep -v "$GREP_STRING" | sed $SEDFLAG -e 's/\$//g' -e 's/'"$SED_NO_PRINTGCDATESTAMPS"'//g'  > $OUTPUT_FILENAME
 fi
