ddd59b1ab1d205de5165bf593d20435f7f7ace75
Jacob Vigeveno <jacob.vigeveno@jivesoftware.com>
2014-12-29 15:10:43 -0800

    Match gc logs with .0 extension

--- fixhpmeter-completion.bash
+++ fixhpmeter-completion.bash
@@ -3,6 +3,6 @@ _fixhpmeter()
 	local cur prev opts
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
-	COMPREPLY=($( compgen -f -X '!*-gc*.log' -- ${cur} ) )
+	COMPREPLY=($( compgen -f -X '!*-gc*.log*' -- ${cur} ) )
 }
 complete -o default -F _fixhpmeter fixhpmeter.sh
