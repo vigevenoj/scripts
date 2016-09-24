0ab0d84ba0ded32a68c0475c0af2dabd746b47a1
Jacob Vigeveno <vigevenoj@gmail.com>
2016-09-23 20:15:27 -0700

    switch up stop ids for trimet arrivals script

--- trimet-arrivals.rb
+++ trimet-arrivals.rb
@@ -15,6 +15,8 @@ require 'nokogiri'
 # 7787 (SW 6th & Pine, for Green)
 # 7782 (SW 6th & Oak, for 12)
 # 8402 (NW Everett & Park, for 77)
+# 2562 (SE 79th & Harold)
+# 1818 (SE Foster & 82nd)
 @stop_list  = [8333, 7787, 7782, 8402]
 
 # stop IDs can be comma-separated list (up to 10)
