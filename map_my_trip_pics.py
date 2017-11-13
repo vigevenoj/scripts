ab6987a2e9ef0901128dbec35fdd8bc9ec472187
Jacob Vigeveno <vigevenoj@gmail.com>
2017-11-13 08:31:07 -0800

    Major updates to trip pics script
    python3 compatibility, output geojson

--- map_my_trip_pics.py
+++ map_my_trip_pics.py
@@ -1,43 +1,56 @@
-"""Script to parse GPS location and time out of photos and return the data as json"""
+"""
+Script to parse GPS location and time out of photos and
+return the data as json
+"""
 import argparse
-import sys
-import os
-import exifread
-import imghdr
-import fractions
 import collections
+import exifread
+import fractions
+import geojson
+# import imghdr
 import json
+import logging
+import os
+import sys
 
 PIC_EXTS = ['.jpg', '.jpeg', '.png']
+
+logger = logging.getLogger(__name__)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 
 class GPS(object):
     def __init__(self, tags):
         self._refs = {'latitude':
-                        1 if str(tags['GPS GPSLatitudeRef']) == 'N' else -1,
+                      1 if str(tags['GPS GPSLatitudeRef']) == 'N' else -1,
                       'longitude':
-                        1 if str(tags['GPS GPSLongitudeRef']) == 'E' else -1}
+                      1 if str(tags['GPS GPSLongitudeRef']) == 'E' else -1}
         self._rawlatitude = tags['GPS GPSLatitude']
         self._rawlongitude = tags['GPS GPSLongitude']
         self._latlong = collections.defaultdict(lambda: None)
+
     @property
     def longitude(self):
         if self._latlong['longitude']:
             return self._latlong['longitude']
         self._compute()
         return self._latlong['longitude']
+
     @property
     def latitude(self):
         if self._latlong['latitude']:
             return self._latlong['latitude']
         self._compute()
         return self._latlong['latitude']
+
     def _compute(self):
         for i, j in {'latitude': self._rawlatitude,
                      'longitude': self._rawlongitude}.items():
             degs, mins, secs = ([float(fractions.Fraction(str(x)))
-                                  for x in j.values[0:3]])
+                                 for x in j.values[0:3]])
             sign = self._refs[i]
             self._latlong[i] = sign*(degs+(mins/60.0)+(secs/3600.0))
+
 
 class PhotoGPS(object):
     ''' Class for finding and presenting GPS data in photos
@@ -47,37 +60,54 @@ class PhotoGPS(object):
         self._dict = {}
         self._gps = None
         self._tags = None
+
     @property
     def tags(self):
         if self._tags:
             return self._tags
-        with open(self._path, 'r') as photo:
+        with open(self._path, 'rb') as photo:
             self._tags = exifread.process_file(photo)
         return self._tags
+
     @property
     def dict(self):
         if self._dict:
             return self._dict
         try:
-            self._dict =  {'filename': self._path,
-                           'latitude': self.latitude,
-                           'longitude': self.longitude}
+            self._dict = {'filename': self._path,
+                          'latitude': self.latitude,
+                          'longitude': self.longitude}
         except (IOError, KeyError):
-            print 'Unable to determine gps data for %s' % self._path
+            logger.error('Unable to determine gps data for %s' % self._path)
             return None
         return self._dict
+
+    def as_geojson(self):
+        """
+        Return the photo as a geojson feature located at the GPS coordinates
+        and with the filename and path as properties
+        """
+        point = geojson.Point((self.longitude, self.latitude))
+        feature = geojson.Feature(geometry=point,
+                                  properties={
+                                      'filename': self._path.split('/')[-1],
+                                      'path': self._path})
+        return feature
+
     @property
     def latitude(self):
         if self._gps:
             return self._gps.latitude
         self._gps = GPS(self.tags)
         return self._gps.latitude
+
     @property
     def longitude(self):
         if self._gps:
             return self._gps.longitude
         self._gps = GPS(self.tags)
         return self._gps.longitude
+
 
 def main():
     """Main entry point for script"""
@@ -91,13 +121,13 @@ def main():
     args = parser.parse_args()
 
     gps_info_for_files = []
-    files = []
+    collection = []
     if not os.path.isdir(args.source):
-        sys.exit(path + " is not a directory")
+        sys.exit(args.source + " is not a directory")
     for(dirpath, dirnames, filenames) in os.walk(args.source):
         for filename in filenames:
             _, ext = os.path.splitext(filename)
-            if ext in PIC_EXTS:
+            if ext.lower() in PIC_EXTS:
                 abspath = os.path.join(dirpath, filename)
             else:
                 continue
@@ -105,7 +135,15 @@ def main():
             (gps_info_for_files.append(gpsdata.dict)
                 if gpsdata.dict
                 else False)
-    print json.dumps(gps_info_for_files)
+            if gpsdata.dict:
+                collection.append(gpsdata.as_geojson())
+    # print(json.dumps(gps_info_for_files))
+    fc = geojson.FeatureCollection(collection)
+    logger.warn(geojson.dumps(fc))
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
     sys.exit(main())
